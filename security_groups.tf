7d2aa58b4176ab2860aeae623313843534947391
Hans Kristian Moen <hans@reserved-192-168-1-4.rchland.ibm.com>
2018-04-13 18:15:27 +0100

    allow ssh within group

--- security_groups.tf
+++ security_groups.tf
@@ -19,3 +19,13 @@ resource "openstack_networking_secgroup_rule_v2" "web" {
   remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = "${openstack_networking_secgroup_v2.frontend.id}"
 }
+
+resource "openstack_networking_secgroup_rule_v2" "ssh" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_group_id   = "${openstack_networking_secgroup_v2.frontend.id}"
+  security_group_id = "${openstack_networking_secgroup_v2.frontend.id}"
+}
